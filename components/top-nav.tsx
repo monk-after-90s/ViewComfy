357e070b58b461b187205fda05b012a83876ff10
antasann <907333918@qq.com>
2025-01-17 11:13:10 +0800

    颜色调整

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -2,7 +2,7 @@ import {ModeToggle} from "./toggle";
 
 export function TopNav() {
     return (
-        <nav className="flex items-center justify-between px-4 py-2 bg-background border-b gap-2 bg-[rgb(65,43,200)]">
+        <nav className="flex items-center justify-between px-4 py-2 bg-background border-b gap-2 bg-[rgb(65,43,199)]">
             <div className="flex items-center">
                 <a href="https://www.uevip.net/" target="_blank" rel="noopener noreferrer">
                     <img src="https://www.uevip.net/img/logo.png" alt="优一科技" className="ml-2 h-8 w-auto"/>
